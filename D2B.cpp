88c41fa48e8c1bc1244be9c4c959a00c91654211
utkarsh pathak <76955280+OLDHel1Otrope@users.noreply.github.com>
2023-09-24 16:26:46 +0530

    Add files via upload

--- D2B.cpp
+++ D2B.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include<math.h>
 using namespace std;
 int main(){
     int binary_fom=0;
@@ -7,14 +8,17 @@ int main(){
     cout<<"decimal number";
     cin>>number;
     x=number;
+    int i=1;
     while(x!=0){
         if(x%2==0){
             remainder=0;
         }
         else remainder=1;
-    binary_fom=(10*binary_fom)+remainder;
+    binary_fom=binary_fom+(i*remainder);
+    cout<<x<<"\t"<<remainder<<endl;
+    //cout<<binary_fom;
+    i*=10;
     x/=2;
-    cout<<binary_fom;
     }
 
     cout<<endl <<binary_fom<<"  is the binary form of "<< number;

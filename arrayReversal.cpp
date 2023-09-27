026c3760b3024085ab2bf12e5d95f2b1a176c21b
utkarsh pathak <76955280+OLDHel1Otrope@users.noreply.github.com>
2023-09-27 23:06:36 +0530

    Update arrayReversal.cpp

--- arrayReversal.cpp
+++ arrayReversal.cpp
@@ -1,14 +1,9 @@
 void arrayReversal(int arr[], int size){
     int temp;
-    int odd=0;
-    if(size%2!=0){
-        odd=1;
-    }
     for(int i=0; i<=(size/2)-1;i++ ){
-
         temp=arr[i];
         arr[i]=arr[size-i-1];
         arr[size-i-1]=temp;
-
+        //works for both even and odd length arrays
     }
 }

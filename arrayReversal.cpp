05d4aef06ebcc44e9606cf01f224a61bc0cc133a
utkarsh pathak <76955280+OLDHel1Otrope@users.noreply.github.com>
2023-09-27 23:05:16 +0530

    Update arrayReversal.cpp

--- arrayReversal.cpp
+++ arrayReversal.cpp
@@ -1,1 +1,14 @@
-arr[size-i-1]
+void arrayReversal(int arr[], int size){
+    int temp;
+    int odd=0;
+    if(size%2!=0){
+        odd=1;
+    }
+    for(int i=0; i<=(size/2)-1;i++ ){
+
+        temp=arr[i];
+        arr[i]=arr[size-i-1];
+        arr[size-i-1]=temp;
+
+    }
+}

b8c5b61dc50557875beeef1c5059ca6c3781be40
utkarsh pathak <76955280+OLDHel1Otrope@users.noreply.github.com>
2023-10-01 09:58:40 +0530

    Update binarySearchRecursion.cpp

--- binarySearchRecursion.cpp
+++ binarySearchRecursion.cpp
@@ -1,12 +1,8 @@
 int binarySearch(int arr[], int ln, int rn, int key){
-    if((ln==rn&&arr[ln]!=key)||key>arr[rn]||key<arr[ln]){
-        return -1;
+    if((ln==rn&&arr[ln]!=key)||key>arr[rn]||key<arr[ln]){return -1;
     }
     int mid=ln+(rn-ln)/2;
-    if(arr[mid]==key){return mid;
-    }
-    else if(arr[mid]<=key){ binarySearch(arr, mid+1, rn, key);
-    }
-    else if(arr[mid]>=key){ binarySearch(arr, ln, mid-1, key);
-    }
+    if(arr[mid]==key){return mid;}
+    else if(arr[mid]<=key){ binarySearch(arr, mid+1, rn, key);}
+    else if(arr[mid]>=key){ binarySearch(arr, ln, mid-1, key);}
 }

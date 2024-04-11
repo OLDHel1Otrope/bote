903109419b7337ad95ab792eb887ad2b698e1020
utkarsh pathak <76955280+OLDHel1Otrope@users.noreply.github.com>
2024-04-11 13:09:49 +0530

    Update weird.java

--- weird.java
+++ weird.java
@@ -10,4 +10,14 @@ class HelloWorld {
     public static void main(String[] args) {
         pn(5);
     }
+    public static int fact(int n){
+        if(n==2)return 2;
+        if(n<=1)return 1;
+        return n*fact(n-1);
+    }
+    
+    public static int sumofdigits(int n){
+        if(n==0) return 0;
+        return n%10+sumofdigits(n/10);
+    }
 }

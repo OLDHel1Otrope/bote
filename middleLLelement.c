bef0041b69f72c01e74cbb996450f82b1a7676c7
utkarsh pathak <76955280+OLDHel1Otrope@users.noreply.github.com>
2024-06-01 16:13:42 +0530

    Create middleLLelement.c

---
+++ middleLLelement.c
@@ -0,0 +1,16 @@
+int middleElement(node *list)
+{
+    if (list == NULL)
+        printf("the list is empty");
+    else
+    {
+        node *fast = list;
+        node *slow = list;
+        while (fast->next != NULL && fast->next->next != NULL)
+        {
+            fast = fast->next->next;
+            slow = slow->next;
+        }
+        return slow->data;
+    }
+}

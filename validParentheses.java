724b45d9a1e416fa6fe3a1b84380e8c37a3154d1
utkarsh pathak <76955280+OLDHel1Otrope@users.noreply.github.com>
2024-06-02 11:52:14 +0530

    Create validParentheses.java

---
+++ validParentheses.java
@@ -0,0 +1,26 @@
+import java.util.Stack;
+//this has to be further optimized
+public class validParentheses {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '(' || s.charAt(i) == '{' || s.charAt(i) == '[') {
+                stack.push(s.charAt(i));
+            } else if (s.charAt(i) == ')' || s.charAt(i) == '}' || s.charAt(i) == ']') {
+                if (stack.isEmpty()) {
+                    return false;
+                }
+
+                char top = stack.peek();
+                if ((c == ')' && top == '(') ||
+                    (c == '}' && top == '{') ||
+                    (c == ']' && top == '[')) {
+                    stack.pop();
+                else {
+                    return false;
+                }
+            }
+        }
+        return stack.empty();
+    }
+}

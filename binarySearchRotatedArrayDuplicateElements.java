7a3d60a3704f7b996b62d52901a25f40f1d19b04
utkarsh pathak <76955280+OLDHel1Otrope@users.noreply.github.com>
2024-07-06 11:15:15 +0530

    Update binarySearchRotatedArrayDuplicateElements.java

--- binarySearchRotatedArrayDuplicateElements.java
+++ binarySearchRotatedArrayDuplicateElements.java
@@ -4,7 +4,11 @@ public static int binarySearchRotatedArrayDuplicateElements(int[] arr, int targe
         int mid=start+(end-start)/2;
         while(start<=end){
             if(arr[mid]==target) return mid;
-            if(arr[start]<=arr[mid]){//checks if the left sub array is sorted
+                if (arr[start] == arr[mid] && arr[mid] == arr[end]) {
+                start++;
+                end--;
+            }
+            else if(arr[start]<=arr[mid]){//checks if the left sub array is sorted
                 if(arr[mid]>=target&&arr[start]<=target)end=mid-1;//checks if the element might lie in the left sub array, go to left one
                 else start=mid+1;//if element dosent lie in the left sub array, go to the right one
             } else if (arr[start]>target&&arr[mid]<target)start=mid+1;

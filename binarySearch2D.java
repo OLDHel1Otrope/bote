4f080d5e92104b43151c4e9829e80a64b989a19f
utkarsh pathak <76955280+OLDHel1Otrope@users.noreply.github.com>
2024-07-06 19:15:22 +0530

    Create binarySearch2D.java

---
+++ binarySearch2D.java
@@ -0,0 +1,12 @@
+    public static int[] binarySearch2D(int[][] arr, int target){
+
+        int rows=arr.length-1, cols=arr[0].length;
+        int row=0,col=cols-1;
+        while(row<=rows&&col>-1){
+            if(arr[row][col]==target)return new int[]{row,col};
+            if(target>arr[row][col])row++;
+            else col--;
+            System.out.println(row+" "+col);
+        }
+        return new int[]{-1,-1};
+    }
